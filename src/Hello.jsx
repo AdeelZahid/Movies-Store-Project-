755b0f60da5bb898168be3bd4210fcb16cb2dd4f
Adeel Zahid <m.adeel2144@gmail.com>
2020-08-04 13:16:41 +0500

    Starter Setup is Ready ..

---
+++ src/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+    return (
+        <div>
+            <div className="Wrapper">
+                <h2>Hello World </h2>
+            </div>
+        </div>
+    )
+}
+
+export default Hello
