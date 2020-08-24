975f912f499a8224235bc38fc7ee164bf46cd0b7
Adeel Zahid <m.adeel2144@gmail.com>
2020-08-24 19:57:42 +0500

    Routing is Almost done !!!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'; 
+import {Route, Redirect, Switch} from 'react-router-dom';
+import Movies from './components/movies';
+import MovieForm from './components/movieForm';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/not-found';
+import NavBar from './components/navBar';
 import "./App.css";
- import Movies from './components/movies';
 
 
 
@@ -8,7 +14,22 @@ class App extends Component {
     state = {  }
     render() { 
         return ( 
-            <Movies />
+            <React.Fragment>
+            <NavBar />
+            <main className="container">
+              <Switch>
+                  
+                <Route path="/movies/:id" component={MovieForm} />
+                <Route path="/movies" component={Movies} />
+                <Route path="/customers" component={Customers}> </Route>
+                <Route path="/rentals" component={Rentals}> </Route>
+                <Route path="/not-found" component={NotFound}> </Route>
+                <Redirect from="/" exact to="/movies" />
+                <Redirect to="/not-found" />
+
+              </Switch>
+            </main>
+          </React.Fragment>
          );
     }
 }
